4126c98829bd00b1ea70f288b0e9708f29a051c1
Robert Ellis <robert.ellis@rightmove.co.uk>
2018-10-23 14:13:28 +0100

    Renamed prefix to xen and root component

---
+++ client/app/root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xen-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.css']
+})
+export class RootComponent {
+  title = 'xenharmonics';
+}
--- client/app/root.module.ts
+++ client/app/root.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { RootComponent } from './root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
-export class AppModule { }
+export class RootModule { }
