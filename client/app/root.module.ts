04d547bc0718f4951c393e0792d8a1fc229b39b6
Robert Ellis <robert.ellis@rightmove.co.uk>
2018-11-12 13:52:00 +0000

    Added tab bar

--- client/app/root.module.ts
+++ client/app/root.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {MatButtonModule, MatTabsModule} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { RootComponent } from './root.component';
+
 
 @NgModule({
   declarations: [
     RootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    RouterModule.forRoot([]),
+    MatTabsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [RootComponent]
