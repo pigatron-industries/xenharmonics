374de9416ec7c93d4a2d8f65b24bc62e259b8fac
Robert Ellis <robert.ellis@rightmove.co.uk>
2018-12-17 16:47:36 +0000

    Added default values in scale constructor

--- client/app/scales/Scale.ts
+++ client/app/scales/Scale.ts
@@ -4,7 +4,12 @@ export class Scale {
   id: number;
   name: string;
   description: string;
-  notesCents: number[] = [];
+  notesCents: number[];
   octaveCents: number;
+
+  constructor() {
+    this.notesCents = [0];
+    this.octaveCents = 1200;
+  }
 
 }
