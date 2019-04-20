ed09ca701d38e6bec7bd78259fb1415d52ca7071
rob <rob@pigatron-industries.co.uk>
2019-04-20 13:15:04 +0100

    Set config to null if there is no hardware channel to map to midi channel

--- server/service/app/ConfigService.ts
+++ server/service/app/ConfigService.ts
@@ -43,14 +43,14 @@ export class ConfigService implements OnInit {
       const defaultChannelConfig = new ChannelConfig();
       defaultChannelConfig.midiChannel = i;
 
-      if (hwConfig.dacOutputChannels <= i) {
+      if (i <= hwConfig.dacOutputChannels) {
         defaultChannelConfig.noteVoltageChannel = i;
       } else {
         defaultChannelConfig.noteVoltageChannel = null;
       }
       defaultChannelConfig.noteVoltageStart = 0;
 
-      if (hwConfig.gateOutputChannels <= i) {
+      if (i <= hwConfig.gateOutputChannels) {
         defaultChannelConfig.gateChannel = i;
       } else {
         defaultChannelConfig.gateChannel = null;
